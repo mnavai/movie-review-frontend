10b18a95e7f1e28695136dcf8de7c64758af82bb
Maryam Navaei <maryam.navaei@gmail.com>
2023-12-15 22:46:10 -0500

    added useState and useEffect

--- src/API/axiosConfiig.js
+++ src/API/axiosConfiig.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-export default axios.create({
-    baseURL: 'https://9c96-103-106-239-104.ap.ngrok.io/',
-    headers: {"ngrok-skip-browser-warning": "true"}
+const api = axios.create({
+  baseURL: 'https://57fa-2603-6080-6300-af37-1030-4241-8430-518d.ngrok-free.app',
+  headers: {"ngrok-skip-browser-warning": "true"}
 });
+
+export default api;
