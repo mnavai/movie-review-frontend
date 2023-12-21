2b159647be94512e44935a2bbe395de8bcbfde50
Maryam Navaei <maryam.navaei@gmail.com>
2023-12-21 13:54:23 -0500

    added trailer preview

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -4,6 +4,7 @@ import Carousel from 'react-material-ui-carousel';
 import { Paper } from '@mui/material';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCirclePlay } from '@fortawesome/free-solid-svg-icons';
+import { Link } from 'react-router-dom';
 
 const Hero = ({ movies }) => {
   return (
@@ -21,10 +22,12 @@ const Hero = ({ movies }) => {
                     <h4>{movie.title}</h4>
                   </div>
                   <div className='movie-buttons-container'>
-                    <div className='play-button-icon-container'>
-                      <FontAwesomeIcon className='play-button-icon'
-                      icon={faCirclePlay} />
-                    </div>
+                    <Link to={`/Trailer/${movie.trailerLink.substring(movie.trailerLink.length - 11)}`}>
+                      <div className='play-button-icon-container'>
+                        <FontAwesomeIcon className='play-button-icon'
+                        icon={faCirclePlay} />
+                      </div>
+                    </Link>
                   </div>
                 </div>
               </div>
