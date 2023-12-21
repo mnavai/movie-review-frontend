40650d811896c22f80bed586b6e46e84c5a2963e
Maryam Navaei <maryam.navaei@gmail.com>
2023-12-21 12:04:10 -0500

    adding style

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -12,10 +12,10 @@ const Hero = ({ movies }) => {
                     return(
                         <Paper>
                             <div className='movie-card-container'>
-                                <div className='movie-card'>
+                                <div className='movie-card' style={{"--img": `url(${movie.backdrops[0]})`}}>
                                     <div className='movie-detail'>
                                         <div className='movie-poster'>
-                                            <img src={movie.poster} alt='' />
+                                            <img src={movie.poster} alt='poster' />
                                         </div>
                                         <div className='movie-title'>
                                             <h4>{movie.title}</h4>
